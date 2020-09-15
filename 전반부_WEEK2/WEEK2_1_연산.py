5f2dce275385e6e7b9bb89b98f60bb1214d40a1e
Jaehyang <67912579+Jaehyang@users.noreply.github.com>
2020-09-16 00:33:40 +0900

    Create WEEK2_1_연산.py

---
+++ 전반부_WEEK2/WEEK2_1_연산.py
@@ -0,0 +1,26 @@
+#숫자의 연산
+a = 1
+b = 5
+print(a+b)
+
+c = a - b
+print(c)
+
+c = b % 2
+print(c)
+
+c = b // 2
+print(c)
+
+# 문자의 연산
+a = "Hello"
+b = "World"
+print(a+b)
+print(a*3)
+
+# 문자와 숫자 더하기
+
+a = 100
+b = "원"
+a = str(a)
+print(a + b)
